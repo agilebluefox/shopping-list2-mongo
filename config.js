a2c8e9e7efc474a8220e9ed5999458d6d467a6c4
David Conner <agilebluefox@gmail.com>
2016-07-27 17:27:29 -0400

    Fixed error in database connection uri

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL || (process.env.NODE_ENV === 'production' ?
-        'mongodb://mongodb://user:33tFkdASta4OI2hV1m@ds031845.mlab.com:31845/agilebluefox-shopping-li' :
+        'mongodb://user:33tFkdASta4OI2hV1m@ds031845.mlab.com:31845/agilebluefox-shopping-li' :
         'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
