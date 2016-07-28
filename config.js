0dd933b6502a6501ada63cbaad3abe2554540d69
David Conner <agilebluefox@gmail.com>
2016-07-28 12:39:03 -0400

    Changed database uri

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL || (process.env.NODE_ENV === 'production' ?
         ENV['PROD_MONGODB'] :
-        'mongodb://localhost/shopping-list-dev');
+        'mongodb://127.0.0.1/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
