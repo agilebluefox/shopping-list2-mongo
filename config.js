08a5cfad41cf3dbdb4810c4b90c1fb1b339087a3
David Conner <agilebluefox@gmail.com>
2016-07-28 12:31:39 -0400

    Added config file intending to fix issues with Travis.

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+    global.DATABASE_URL || (process.env.NODE_ENV === 'production' ?
+        ENV['PROD_MONGODB'] :
+        'mongodb://localhost/shopping-list-dev');
+exports.PORT = process.env.PORT || 8080;
